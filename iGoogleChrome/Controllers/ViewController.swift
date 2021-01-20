29f85308ccb275e8aa672f2ece7a2de3ba12dffa
Alexis <roboform@hotmail.fr>
2021-01-20 16:17:49 +0100

    Picker + TabNav + NavBar

--- iGoogleChrome/Controllers/ViewController.swift
+++ iGoogleChrome/Controllers/ViewController.swift
@@ -8,12 +8,125 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var currencyInTextField: UITextField!
+    @IBOutlet weak var currencyOutTextField: UITextField!
+    @IBOutlet weak var amountTextField: UITextField!
+    @IBOutlet weak var dateStartDatePicker: UIDatePicker!
+    @IBOutlet weak var dateEndDatePicker: UIDatePicker!
+    
+    @IBOutlet weak var convertButton: UIButton!
+    
+    private weak var currentInput: UITextField?
+    
+    private var items: [String] = ["Item 1", "Item 2", "Item 3", "Item 4"]
+    private var availableList: [String] = []
+    
+    private var pickerIn: UIPickerView = UIPickerView()
+    private var pickerOut: UIPickerView = UIPickerView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let date = Date()
+        dateEndDatePicker.maximumDate = date
+        dateStartDatePicker.maximumDate = date
+        
+        // Picker 1
+        pickerIn.dataSource = self
+        pickerIn.delegate = self
+        // Picker 2
+        pickerOut.dataSource = self
+        pickerOut.delegate = self
+        
+        currencyInTextField.delegate = self
+        currencyOutTextField.delegate = self
+        
+        currencyInTextField.inputView = pickerIn
+        currencyOutTextField.inputView = pickerOut
+        
+        dismissPickerView(field: currencyInTextField)
+        dismissPickerView(field: currencyOutTextField)
+    }
+    
+    func dismissPickerView(field: UITextField) {
+       let toolBar = UIToolbar()
+       toolBar.sizeToFit()
+       let button = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(self.action))
+       toolBar.setItems([button], animated: true)
+       toolBar.isUserInteractionEnabled = true
+       field.inputAccessoryView = toolBar
+    }
+    
+    @objc func action() {
+          view.endEditing(true)
+    }
+    
+    @IBAction func onClick() {
+        showDialog(title: "Coucou", message: "C'est un test")
+    }
+    
+    func showDialog(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { action in
+              switch action.style{
+              case .default:
+                    print("default")
+
+              case .cancel:
+                    print("cancel")
+
+              case .destructive:
+                    print("destructive")
+
+
+        }}))
+        self.present(alert, animated: true, completion: nil)
     }
 
+}
 
+extension ViewController: UIPickerViewDelegate {
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        guard let inputField = currentInput else {
+            return
+        }
+        inputField.text = availableList[row]
+    }
+    
+}
+
+extension ViewController: UIPickerViewDataSource {
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return availableList.count
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return availableList[row]
+    }
+    
+}
+
+extension ViewController: UITextFieldDelegate {
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        currentInput = textField
+        weak var otherInput = (textField == currencyInTextField) ? currencyOutTextField : currencyInTextField
+        let filtered = items.filter { item in
+            return item != otherInput?.text
+        }
+        
+        availableList = filtered
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        currentInput = nil
+    }
+    
 }
 
